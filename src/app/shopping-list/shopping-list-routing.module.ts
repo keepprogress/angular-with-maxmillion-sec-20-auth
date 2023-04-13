c8db4d62ad6b4e71a8f613a338e97751aab73091
Nick.Hsu <Nick.Hsu@testritegroup.com>
2023-04-13 13:28:46 +0800

    Sec.22 326. The ShoppingList Feature Module

---
+++ src/app/shopping-list/shopping-list-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { ShoppingListComponent } from './shopping-list.component';
+import { RouterModule } from '@angular/router';
+
+const routes = [
+   {
+     path: '',
+     children: [
+       { path: 'shopping-list', component: ShoppingListComponent },
+     ]
+   },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ShoppingListRoutingModule {}
